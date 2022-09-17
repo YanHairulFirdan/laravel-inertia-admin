976b49e7865623f86e0381ba381a07fc1312b784
yan_backend <yanhairulfirdan@gmail.com>
2022-09-17 17:58:21 +0800

    make custom component for show validation error message

---
+++ resources/js/Components/UI/Form/ValidationError.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-danger text-small">
+    {{ props.message }}
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    message:  String
+  })
+</script>
