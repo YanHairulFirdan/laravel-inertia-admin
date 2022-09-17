e5f3973bca0171049997760b5040dafbca254571
yan_backend <yanhairulfirdan@gmail.com>
2022-09-17 09:42:38 +0800

    fix code indentation

--- resources/js/Components/Stisla/BaseLayout.vue
+++ resources/js/Components/Stisla/BaseLayout.vue
@@ -1,34 +1,35 @@
 <template>
-    <div class="main-wrapper">
-        <div class="navbar-bg"></div>
-        <Navbar></navbar>
-        <div class="main-sidebar sidebar-style-2">
-            <aside id="sidebar-wrapper">
-                <div class="sidebar-brand">
-                    <a href="index.html">Stisla</a>
-                </div>
-                <div class="sidebar-brand sidebar-brand-sm">
-                    <a href="index.html">St</a>
-                </div>
-                <Sidebar></Sidebar>
-            </aside>
+  <div class="main-wrapper">
+    <Head title="Base layout"/>
+    <div class="navbar-bg"></div>
+    <Navbar></Navbar>
+    <div class="main-sidebar sidebar-style-2">
+      <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+          <a href="index.html">Stisla</a>
         </div>
-
-        <!-- Main Content -->
-        <div class="main-content">
-            <section class="section">
-                <slot />
-            </section>
+        <div class="sidebar-brand sidebar-brand-sm">
+          <a href="index.html">St</a>
         </div>
-        <footer class="main-footer">
-            <div class="footer-left">
-                Copyright &copy; 2018
-                <div class="bullet"></div>
-                Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
-            </div>
-            <div class="footer-right"></div>
-        </footer>
+        <Sidebar></Sidebar>
+      </aside>
     </div>
+
+    <!-- Main Content -->
+    <div class="main-content">
+      <section class="section">
+        <slot />
+      </section>
+    </div>
+    <footer class="main-footer">
+      <div class="footer-left">
+        Copyright &copy; 2018
+        <div class="bullet"></div>
+        Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+      </div>
+      <div class="footer-right"></div>
+    </footer>
+  </div>
 </template>
 <script setup>
 import { onMounted } from "@vue/runtime-core";
@@ -37,12 +38,12 @@ import Navbar from "./Navbar.vue";
 import { asset } from "@/functions";
 
 onMounted(() => {
-    const stislaScript = document.createElement('script')
-    stislaScript.setAttribute('src', asset('stisla-master/assets/js/stisla.js'))
-    const script = document.createElement('script')
-    script.setAttribute('src', asset('stisla-master/assets/js/scripts.js'))
+  const stislaScript = document.createElement("script");
+  stislaScript.setAttribute("src", asset("stisla-master/assets/js/stisla.js"));
+  const script = document.createElement("script");
+  script.setAttribute("src", asset("stisla-master/assets/js/scripts.js"));
 
-    document.body.appendChild(stislaScript)
-    document.body.appendChild(script)
-})
+  document.body.appendChild(stislaScript);
+  document.body.appendChild(script);
+});
 </script>
