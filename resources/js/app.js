32a7be12c3757f7b598071cedc4699701b062d93
yan_backend <yanhairulfirdan@gmail.com>
2022-09-20 16:40:07 +0800

    make global property $route to available in all template

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import BaseLayout from "@/Components/Stisla/BaseLayout.vue";
+import route from 'ziggy-js';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -14,11 +15,14 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+        const app = createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .component('base-layout', BaseLayout)
-            .mount(el);
+        
+        app.config.globalProperties.$route = route;
+
+        return app.mount(el);
     },
 });
 
