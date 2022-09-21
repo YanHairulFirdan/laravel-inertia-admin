66667d8b74194814b2f483036173b85b156fe9a3
yan_backend <yanhairulfirdan@gmail.com>
2022-09-21 10:57:40 +0800

    fix conflict variable name

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import BaseLayout from "@/Components/Stisla/BaseLayout.vue";
-import route from 'ziggy-js';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -15,14 +14,14 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
-        const app = createApp({ render: () => h(app, props) })
+        const vue = createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .component('base-layout', BaseLayout)
         
-        app.config.globalProperties.$route = route;
+        vue.config.globalProperties.$route = route;
 
-        return app.mount(el);
+        return vue.mount(el);
     },
 });
 
