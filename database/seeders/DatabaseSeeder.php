9ee1b8df4551180597109c7ef00d2151fb02c6e3
yan_backend <yanhairulfirdan@gmail.com>
2022-09-26 13:12:46 +0800

    call default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +18,15 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'my@mail.com',
+            'password' => 'userpass',
+        ]);
+
+        $this->call([
+            LanguageSeeder::class,
+            PostSeeder::class,
+        ]);
     }
 }
